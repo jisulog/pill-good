ea720f2e068057ad7760a07fe766657c148e86ef
jyeoneely <86949874+jyeoneely@users.noreply.github.com>
2021-10-06 17:17:15 +0900

    member app create

---
+++ pillgood/member/views.py
@@ -0,0 +1,30 @@
+from django.http import HttpResponse
+
+
+# Create your views here.
+def main(request):
+    return HttpResponse("개인정보화면입니다")
+
+
+def update():
+    pass
+
+
+def delete():
+    pass
+
+
+def paylist():
+    pass
+
+
+def lec():
+    pass
+
+
+def pay_refund():
+    pass
+
+
+def lec_delete():
+    pass
