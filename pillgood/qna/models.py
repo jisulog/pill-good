aa1a798195129e432e64038264ba5ba7b9be2e06
jisulog <jisu.rok@gmail.com>
2021-10-07 18:45:27 +0900

    Modify models

--- pillgood/qna/models.py
+++ pillgood/qna/models.py
@@ -1,13 +1,17 @@
+from django.conf import settings
 from django.db import models
 from django.utils import timezone
+
+
 # Create your models here.
 
 class Qna(models.Model):
-    qna_id = models.IntegerField(primary_key=True)          #상담pk
-    title = models.CharField(max_length=100)                #제목
-    question_user = models.CharField(max_length=30)         #작성자(회원)
-    question = models.TextField()                           #내용
-    answer_user = models.CharField(null=True)               #답변자(회원)
-    answer = models.TextField(null=True)                    #답변
-    date = models.DateTimeField(default=timezone.now)       #등록일
+    object = models.Manager()
 
+    qna_id = models.IntegerField(primary_key=True)  # 상담pk
+    title = models.CharField(max_length=100)  # 제목
+    question_user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='question_user')  # 작성자(회원)
+    question = models.TextField()  # 내용
+    answer_user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='answer_user', null=True)  # 답변자(회원)
+    answer = models.TextField(null=True)  # 답변
+    date = models.DateTimeField(default=timezone.now)  # 등록일
