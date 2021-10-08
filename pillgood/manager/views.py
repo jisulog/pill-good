139235e635bf7b57a17d0d746afac8c1a77875bd
jisulog <jisu.rok@gmail.com>
2021-10-08 11:05:05 +0900

    Modify manager views and serializers, models

--- pillgood/manager/views.py
+++ pillgood/manager/views.py
@@ -1,6 +1,8 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from manager.serializer import MembershipSerializer, UserSerializer, LecSerializer, PaySerializer
+from user.serializers import UserSerializer
+from lec.serializer import LecSerializer
+from membership.serializers import MembershipSerializer, PaySerializer
 from user.models import User
 from lec.models import Lec
 from membership.models import Pay
@@ -8,13 +10,13 @@ from membership.models import Pay
 
 @api_view(['GET'])
 def manager_user(request):
-    users = User.object.all()
+    users = User.objects.all()
     serializer = UserSerializer(users, many=True)
     return Response(serializer.data)
 
 @api_view(['PUT'])
 def manager_user_access(request, pk):
-    user = User.object.get(id=pk)
+    user = User.objects.get(id=pk)
     serializer = UserSerializer(instance=user, data=request.data)
     if serializer.is_valid():
         serializer.save()
@@ -22,25 +24,25 @@ def manager_user_access(request, pk):
 
 @api_view(['GET'])
 def manager_user_detail(request, pk):
-    user = User.object.get(id=pk)
+    user = User.objects.get(id=pk)
     serializer = UserSerializer(user, many=False)
     return Response(serializer.data)
 
 @api_view(['GET'])
 def manager_lec(request):
-    lecs = Lec.object.all()
+    lecs = Lec.objects.all()
     serializer = LecSerializer(lecs, many=True)
     return Response(serializer.data)
 
 @api_view(['GET'])
 def manager_lec_detail(request, pk):
-    lec = Lec.object.get(id=pk)
+    lec = Lec.objects.get(id=pk)
     serializer = LecSerializer(lec, many=False)
     return Response(serializer.data)
 
 @api_view(['PUT'])
 def manager_lec_access(request, pk):
-    lec = Lec.object.get(id=pk)
+    lec = Lec.objects.get(id=pk)
     serializer = LecSerializer(instance=lec, data=request.data)
     if serializer.is_valid():
         serializer.save()
@@ -48,7 +50,7 @@ def manager_lec_access(request, pk):
 
 @api_view(['PUT'])
 def manager_lec_reject(request, pk):
-    lec = Lec.object.get(id=pk)
+    lec = Lec.objects.get(id=pk)
     serializer = LecSerializer(instance=lec, data=request.data)
     if serializer.is_valid():
         serializer.save()
@@ -56,7 +58,7 @@ def manager_lec_reject(request, pk):
 
 @api_view(['PUT'])
 def manager_lec_update(request, pk):
-    lec = Lec.object.get(id=pk)
+    lec = Lec.objects.get(id=pk)
     serializer = LecSerializer(instance=lec, data=request.data)
     if serializer.is_valid():
         serializer.save()
@@ -64,7 +66,7 @@ def manager_lec_update(request, pk):
 
 @api_view(['DELETE'])
 def manager_lec_delete(request, pk):
-    lec = Lec.object.get(id=pk)
+    lec = Lec.objects.get(id=pk)
     lec.delete()
     return Response('Delete Lec')
 
@@ -77,19 +79,19 @@ def manager_membership(request):
 
 @api_view(['GET'])
 def manager_refund(request):
-    pays = Pay.object.all()
+    pays = Pay.objects.all()
     serializer = PaySerializer(pays, many=True)
     return Response(serializer.data)
 
 @api_view(['GET'])
 def manager_refund_detail(request, pk):
-    pay = Pay.object.get(id=pk)
+    pay = Pay.objects.get(id=pk)
     serializer = PaySerializer(pay, many=False)
     return Response(serializer.data)
 
 @api_view(['PUT'])
 def manager_refund_access(request, pk):
-    pay = Pay.object.get(id=pk)
+    pay = Pay.objects.get(id=pk)
     serializer = PaySerializer(instance=pay, data=request.data)
     if serializer.is_valid():
         serializer.save()
@@ -97,7 +99,7 @@ def manager_refund_access(request, pk):
 
 @api_view(['PUT'])
 def manager_refund_reject(request, pk):
-    pay = Pay.object.get(id=pk)
+    pay = Pay.objects.get(id=pk)
     serializer = PaySerializer(instance=pay, data=request.data)
     if serializer.is_valid():
         serializer.save()
