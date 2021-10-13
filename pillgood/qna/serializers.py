510aae2f623776c542822cd7773847835a40f066
ehssk3239 <ehssk3239@gmail.com>
2021-10-13 09:10:02 +0900

    pay

--- pillgood/qna/serializers.py
+++ pillgood/qna/serializers.py
@@ -2,10 +2,15 @@ from rest_framework import serializers
 from .models import Qna
 
 
-class QnaSerializer(serializers.HyperlinkedModelSerializer):
+class QnaSerializer(serializers.ModelSerializer):
+    answer_user = serializers.PrimaryKeyRelatedField(allow_null=True, read_only=True)
+    answer = serializers.CharField(required=False, allow_blank=True)
+
     class Meta:
         model = Qna
-        fields = ['qna_id', 'title', 'question_user', 'question', 'answer_user', 'answer', 'date']
+        fields = ['qna_id', 'title', 'question_user', 'question', 'answer_user', 'answer']
 
-
-
+    def get_answer(self, instance):
+        serializers = self.__class__(instance.answer, many=True)
+        serializers.bind('', self)
+        return serializers.data
--- pillgood/qna/views.py
+++ pillgood/qna/views.py
@@ -10,50 +10,66 @@ from .serializers import QnaSerializer
 
 @api_view(['GET'])
 def qna_index(request):
-    qna = Qna.objects.all()
-    serializer = QnaSerializer(qna, many=True)
+    qnas = Qna.objects.all()
+    serializer = QnaSerializer(qnas, many=True)
     return Response(serializer.data)
 
 
 @api_view(['GET'])
 def qna_detail(request, pk):
-    qna = Qna.objects.get(id=pk)
-    serializer = QnaSerializer(qna, many=True)
-    return Response(serializer.data)
+    if request.user.is_authenticated:
+        qna = Qna.objects.get(qna_id=pk)
+        serializer = QnaSerializer(qna, many=True)
+        return Response(serializer.data)
+    else:
+        return Response({"message": "로그인 해주세요!"})
 
 
 @api_view(['POST'])
 def qna_create(request):
-    serializer = QnaSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response({"message": "Success!"})
+    if request.user.is_authenticated:
+        serializer = QnaSerializer(data=request.data)
+        print(serializer)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
     else:
-        return Response({"message": "data not valid!"})
+        return Response({"message": "로그인 해주세요!"})
 
 
 @api_view(['PUT'])
 def qna_update(request, pk):
-    qna = Qna.objects.get(id=pk)
-    serializer = QnaSerializer(instance=qna, data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response({"message": "Success!"})
+    if request.user.is_authenticated:
+        qna = Qna.objects.get(qna_id=pk)
+        serializer = QnaSerializer(instance=qna, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+    else:
+        return Response({"message": "로그인 해주세요!"})
 
 
 @api_view(['DELETE'])
-def qna_delete(pk):
-    qna = Qna.objects.get(id=pk)
-    qna.delete()
-    return Response({"message": "Success!"})
+def qna_delete(request, pk):
+    if request.user.is_authenticated:
+        qna = Qna.objects.get(qna_id=pk)
+        qna.delete()
+        return Response(serializer.data)
+    else:
+        return Response({"message": "로그인 해주세요!"})
 
 
 @api_view(['POST'])
 def qna_answer(request, pk):
-    qna = Qna.objects.get(id=pk)
-    serializer = QnaSerializer(instance=qna, data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response({"message": "Success!"})
+    if request.user.is_authenticated:
+        qna = Qna.objects.get(qna_id=pk)
+        serializer = QnaSerializer(instance=qna, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response({"message": "오류! 확인 후 다시 시도해주세요."})
     else:
-        return Response({"message": "data not valid!"})
+        return Response({"message": "로그인 해주세요!"})
