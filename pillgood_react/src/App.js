20a77190aeed7a8cac93a74e9a4e2c7b4e68d294
ehssk3239 <ehssk3239@gmail.com>
2021-10-15 15:24:08 +0900

    membership 211015

--- pillgood_react/src/App.js
+++ pillgood_react/src/App.js
@@ -2,7 +2,13 @@ import React, { Component } from "react";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import Home from "./layout/Home";
 import MemberMainPage from "./pages/MemberMainPage";
-
+import MembershipMainPage from "./membership/pages/MembershipMainPage";
+import PayPage from "./membership/pages/PayPage";
+import QnaMainPage from "./qna/pages/QnaMainPage";
+import QnaDetailPage from "./qna/pages/QnaDetailPage";
+import QnaCreatePage from "./qna/pages/QnaCreatePage";
+import QnaUpdatePage from "./qna/pages/QnaUpdatePage";
+import QnaAnswerPage from "./qna/pages/QnaAnswerPage";
 
 class App extends Component {
     render() {
@@ -11,6 +17,13 @@ class App extends Component {
                 <Switch>
                     <Route exact path="/" component={Home} />
                     <Route exact path="/member" component={MemberMainPage} />
+                    <Route exact path="/membership" component={MembershipMainPage} />
+                    <Route exact path="/membership/pay" component={PayPage} />
+                    <Route exact path="/qna" component={QnaMainPage} />
+                    <Route exact path="/qna/:id" component={QnaDetailPage} />  
+                    <Route exact path="/qna/create/" component={QnaCreatePage} />
+                    <Route exact path="/qna/update/:id" component={QnaUpdatePage} />
+                    <Route exact path="/qna/answer/:id" component={QnaAnswerPage} />
                 </Switch>
             </Router>
         );
