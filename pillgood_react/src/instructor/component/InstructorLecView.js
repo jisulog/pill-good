d202db5aa48849a7086639c94b6b089ae6ea893d
hyunjiin <guswlsdk01@gmail.com>
2021-10-26 16:45:01 +0900

    10.25 hyunjinsong

--- pillgood_react/src/instructor/component/InstructorLecView.js
+++ pillgood_react/src/instructor/component/InstructorLecView.js
@@ -2,28 +2,24 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import {S3_BUCKET, REGION} from '../../image/S3bucket';
 import moment from 'moment';
+import { DataGrid } from '@mui/x-data-grid';
+
 
 class InstructorLecView extends Component {
   render() {
     const {lec}  = this.props;
+    const { columns, rows, useStyles  } = this.props;
+    const classes = useStyles;
 
-    const imgUrl = `https://${S3_BUCKET}.s3.${REGION}.amazonaws.com/${lec.lec_image}`;
       return(
         <div>
-            <div>
-                <img src={imgUrl} alt="강의 사진" width="200"/>
-            </div>
-          <p>
-            <span> ========================</span><br />
+        <DataGrid style={{ height: 500, width: '100%'}}
+            rows={rows}
+            columns={columns}
+            pageSize={5}
+            hideFooterSelectedRowCount={true}
+          />
 
-            강의명 : {lec.title}<br />
-            장소: {lec.room}<br />
-            날짜: {moment(lec.date).format("YY.MM.DD")}}<br />
-            시간: {moment(lec.time,"HH:mm:ss").format("HH:mm")}<br /><br />
-              <Link to= {`/instructor/user/${lec.lec_id}`}><button >신청 회원 목록 </button></Link>&nbsp;&nbsp;
-
-            <br />
-           </p>
         </div>
 
     );
--- pillgood_react/src/lec/component/LecView.js
+++ pillgood_react/src/lec/component/LecView.js
@@ -1,28 +1,49 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import {S3_BUCKET, REGION} from '../../image/S3bucket';
+import ImageList from '@mui/material/ImageList';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+import Modal from '@mui/material/Modal';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+
+
 //lec은 강의 정보를 담은 배열(books와 같음), for each로 돌아가면서 강의 정보하나하나를 element로 받아서 출력.
 class LecView extends Component {
+
 
   render() {
     const {lec}  = this.props;
 
      const imgUrl = `https://${S3_BUCKET}.s3.${REGION}.amazonaws.com/${lec.lec_image}`;
       return(
-        <div>
-          <p>
 
-             <img src={imgUrl} alt="강의 사진" width="200"/><br />
-            강의명 : {lec.title}<br />
-
-         <Link to = {`/lec/${lec.lec_id}`}><button>상세보기</button></Link>
-
-
-           </p>
-        </div>
-
-    );
-  }
-}
+           <Card sx={{ maxWidth: 300 }}>
+           <CardActionArea>
+           <Link to = {`/lec/${lec.lec_id}`}>
+            <CardMedia
+          component="img"
+          height="250"
+          image={imgUrl}
+          alt="강의 사진 "
+        />
+        </Link>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {lec.title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            강사 : {lec.email}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+ );
+}}
 
 export default LecView;
